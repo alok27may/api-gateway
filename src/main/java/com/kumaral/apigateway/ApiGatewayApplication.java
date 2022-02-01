a2e9c6c9626658403f76151df1b9cc99ebd72e23
alok.kumar@sincrodigital.com <alok.kumar@sincrodigital.com>
2022-02-01 18:26:42 +0530

    added discovery client and ribbon aware rest template call for service lookup

--- src/main/java/com/kumaral/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/kumaral/apigateway/ApiGatewayApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients
 @EnableCircuitBreaker
@@ -14,6 +17,12 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 
 }
--- src/main/java/com/kumaral/apigateway/CoolCarController.java
+++ src/main/java/com/kumaral/apigateway/CoolCarController.java
@@ -1,13 +1,21 @@
 package com.kumaral.apigateway;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
@@ -15,12 +23,11 @@ import java.util.stream.Collectors;
 public class CoolCarController {
     private final CarClient carClient;
 
+    @Autowired private DiscoveryClient discoveryClient;
+    @Autowired private RestTemplate restTemplate;
+
     public CoolCarController(CarClient carClient) {
         this.carClient = carClient;
-    }
-
-    private Collection<Car> fallback() {
-        return new ArrayList<>();
     }
 
     @GetMapping("/cool-cars")
@@ -35,9 +42,42 @@ public class CoolCarController {
                 .collect(Collectors.toList());
     }
 
+    @GetMapping("/cool-cars/{name}")
+    public Car getCarByName(@PathVariable final String name) {
+        final List<ServiceInstance> instances = discoveryClient.getInstances("car-service");
+        final String url = instances.get(0).getUri().toString();
+
+        final RestTemplate restTemplateTest = new RestTemplate();
+        final ResponseEntity<Car> exchange =
+                restTemplateTest.exchange(url + "/car/cars/{name}",
+                        HttpMethod.GET, null,
+                        Car.class, name);
+
+        return exchange.getBody();
+    }
+
+    @GetMapping("/car-ribbon/{name}")
+    public Car getRibbonBasedCarByName(@PathVariable final String name) {
+        final ResponseEntity<Car> exchange =
+                restTemplate.exchange("http://car-service/car/cars/{name}",
+                        HttpMethod.GET, null,
+                        Car.class, name);
+
+        return exchange.getBody();
+    }
+
     @GetMapping("/msg")
     public String getGatewayMsg() {
         return "Getting message from Api-Gateway";
+    }
+
+    private Collection<Car> fallback() {
+        final Car testCar = new Car();
+        testCar.setName("TestCar");
+
+        final ArrayList<Car> cars = new ArrayList<>();
+        cars.add(testCar);
+        return cars;
     }
 
     private boolean isCool(Car car) {
